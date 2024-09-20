2de5925dbad0dbc6f26b4a85de46c984101350d4
becouif <lovguya@gmail.com>
2024-09-20 08:39:34 +0300

    updated everything

--- app/routes.py
+++ app/routes.py
@@ -8,7 +8,8 @@ from app.models import Todo
 def index():
     statement = db.select(Todo)
     todos = db.session.scalars(statement).all()
-    return render_template('index.html', title="Home", todos=todos)
+    form = TodoForm()
+    return render_template('index.html', title="Home", todos=todos, form=form)
 
 
 @app.route('/create', methods=['GET', 'POST'])
@@ -22,6 +23,32 @@ def create():
         db.session.add(new_todo)
         db.session.commit()
         flash('Todo={}'.format(form.todo.data))
-        return redirect('/index')
+        return redirect(url_for('index'))
 # this is for get method
     return render_template('create.html', title="create", form=form)
+
+
+@app.route('/edit/<int:id>', methods=['GET', 'POST'])
+def update(id):
+    todo = Todo.query.get_or_404(id)
+    form = TodoForm()
+    if form.validate_on_submit():
+        # update d data
+        todo.todo = form.todo.data
+        db.session.commit()
+        flash('Todo updated successfully!!!')
+        return redirect(url_for('index'))
+
+    # this will prepopulate the field with existing data
+    form.todo.data = todo.todo
+    return render_template('edit.html', title='Edit',form=form)
+
+
+
+@app.route('/delete/<int:id>', methods=['POST'])
+def delete(id):
+    todo = Todo.query.get_or_404(id)
+    db.session.delete(todo)
+    db.session.commit()
+    flash('Todo deleted')
+    return redirect(url_for('index'))
